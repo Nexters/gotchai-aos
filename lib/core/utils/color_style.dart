5283c93d581f6fe507d64ece70c93e2af963742e
plashdof <plashdof@gmail.com>
2025-07-31 19:42:39 +0900

    chore : design system 적용용

---
+++ lib/core/utils/color_style.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class GotchaiColorStyles {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color gray50 = Color(0xFFF7F8FA);
+  static const Color gray100 = Color(0xFFE3E5E9);
+  static const Color gray200 = Color(0xFFC8CAD0);
+  static const Color gray300 = Color(0xFFAEB0B7);
+  static const Color gray400 = Color(0xFF91939B);
+  static const Color gray500 = Color(0xFF767880);
+  static const Color gray600 = Color(0xFF5D5F66);
+  static const Color gray700 = Color(0xFF43454A);
+  static const Color gray800 = Color(0xFF2A2B2F);
+  static const Color gray850 = Color(0xFF232429);
+  static const Color gray900 = Color(0xFF1D1E22);
+  static const Color gray950 = Color(0xFF151519);
+  static const Color black = Color(0xFF000000);
+
+  static const Color primary100 = Color(0xFFF8FFE3);
+  static const Color primary200 = Color(0xFFEAFFAA);
+  static const Color primary300 = Color(0xFFDBFF71);
+  static const Color primary400 = Color(0xFFBFFF00);
+  static const Color primary500 = Color(0xFFAAE300);
+  static const Color primary600 = Color(0xFF7FAA00);
+  static const Color primary700 = Color(0xFF557100);
+  static const Color primary800 = Color(0xFF405500);
+  static const Color primary900 = Color(0xFF2A3900);
+  static const Color primary950 = Color(0xFF151C00);
+}
---
+++ lib/core/utils/text_style.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class GotchaiTextStyles {
+  static const TextStyle title1 = TextStyle(
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 36,
+    color: Colors.white,
+  );
+
+  static const TextStyle title2 = TextStyle(
+    fontFamily: 'PretendardBold',
+    fontSize: 30,
+    color: Colors.white,
+  );
+
+  static const TextStyle title3 = TextStyle(
+    fontFamily: 'PretendardBold',
+    fontSize: 26,
+    color: Colors.white,
+  );
+
+  static const TextStyle title4 = TextStyle(
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 24,
+    color: Colors.white,
+  );
+
+  static const TextStyle subtitle1 = TextStyle(
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 22,
+    color: Colors.white,
+  );
+
+  static const TextStyle subtitle2 = TextStyle(
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 20,
+    color: Colors.white,
+  );
+
+  static const TextStyle body1 = TextStyle(
+    fontFamily: 'PretendardMedium',
+    fontSize: 18,
+    color: Colors.white,
+  );
+
+  static const TextStyle body2 = TextStyle(
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 18,
+    color: Colors.white,
+  );
+
+  static const TextStyle body3 = TextStyle(
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 16,
+    color: Colors.white,
+  );
+
+  static const TextStyle body4 = TextStyle(
+    fontFamily: 'PretendardMedium',
+    fontSize: 16,
+    color: Colors.white,
+  );
+
+  static const TextStyle body5 = TextStyle(
+    fontFamily: 'PretendardMedium',
+    fontSize: 15,
+    color: Colors.white,
+  );
+  static const TextStyle body6 = TextStyle(
+    fontFamily: 'PretendardMedium',
+    fontSize: 14,
+    color: Colors.white,
+  );
+}
